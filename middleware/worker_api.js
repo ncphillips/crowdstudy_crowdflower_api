6c8dfec5eca9bfd5b78470bb709a51ad9ed8cd8c
ncphillips <ncphillips@upei.ca>
2015-08-14 15:29:17 -0300

    main file

--- middleware/worker_api.js
+++ middleware/worker_api.js
@@ -4,28 +4,28 @@
 var Crowdflower = require('nodejs-crowdflower');
 
 module.exports.bonus = function (req, res, next) {
-    var worker = Crowdflower.CrowdWorker(req.apiKey);
+    var worker = new Crowdflower.CrowdWorker(req.apiKey);
     worker.bonus(req.job_id, req.worker_id, req.amount, req.reason, next);
 };
 
 module.exports.reject = function (req, res, next) {
-    var worker = Crowdflower.CrowdWorker(req.apiKey);
+    var worker = new Crowdflower.CrowdWorker(req.apiKey);
     worker.reject(req.job_id, req.worker_id, next);
 };
 
 module.exports.notify = function (req, res, next) {
-    var worker = Crowdflower.CrowdWorker(req.apiKey);
+    var worker = new Crowdflower.CrowdWorker(req.apiKey);
     worker.notify(req.job_id, req.worker_id, req.message, next);
 };
 
 module.exports.flag = function (req, res, next) {
-    var worker = Crowdflower.CrowdWorker(req.apiKey);
+    var worker = new Crowdflower.CrowdWorker(req.apiKey);
     var persist = true;
     worker.flag(req.job_id, req.worker_id, req.reason, persist, next);
 };
 
 module.exports.deflag = function (req, res, next) {
-    var worker = Crowdflower.CrowdWorker(req.apiKey);
+    var worker = new Crowdflower.CrowdWorker(req.apiKey);
     worker.notify(req.job_id, req.worker_id, req.reason, next);
 };
 
